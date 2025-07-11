c9dc8dd5a1279429435df536185eb5cc513b61b5
Raunak Goyal <216869081+raunak-goyal@users.noreply.github.com>
2025-07-11 12:08:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_converOperation.js
@@ -0,0 +1,24 @@
+let score = true
+
+console.log(typeof score);
+
+console.log(typeof (score));
+let valueInNumber =Number(score) //typecasting 
+console.log(typeof valueInNumber);
+console.log(score);
+
+//"33"=>33
+//"33abc0" => NaN
+// true =>1; false => 0
+
+let IsLoggedIn = ""
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+// "" =>false
+// "raunak" =>true
+let somenum = 33
+let stringnum = String(somenum)
+console.log(stringnum);
+console.log(typeof stringnum);
