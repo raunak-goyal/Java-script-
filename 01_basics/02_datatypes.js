8eea6d97797404b5f0dbb4f43dc5e025d05df101
Raunak Goyal <216869081+raunak-goyal@users.noreply.github.com>
2025-07-11 10:59:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+// alert (3+3) //we are using node js .not browser
+console.log(3+3)
+console.log("raunak");
+
+let name = "raunak"//string
+
+let age = 19 //int
+
+let isloggedIn = true //boolean
+
+
+//primitive Datatypes
+//number=> 2 to power 53
+//big int
+//string => true/false
+//null =? standalone value
+//undefined =>
+//symbol => unique 
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
